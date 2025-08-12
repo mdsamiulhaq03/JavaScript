84dfbb1340dfa7db60fd157c276a4ec27f43b0b6
mdsamiulhaq03 <samiultahsin2001@gmail.com>
2025-08-12 22:46:48 +0600

    start day 1

---
+++ problem 1/script.js
@@ -0,0 +1,8 @@
+// console.log("Hello, World!");
+// console.log(2 + 2);
+// console.log("Hello, " + "World!");
+// //console.warn("This is a warning message.");
+// console.clear();
+// console.error("This is an error message.");
+
+//alert("Welcome Buddy");
